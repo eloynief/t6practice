7459c795e0ab1bd80c375ca288f6668ada29ed2a
enieves <enieves@106-02.info2>
2023-02-21 14:21:34 +0100

    terminar ej6 crear comandos

--- Tema6/src/prac/Ej2.java
+++ Tema6/src/prac/Ej2.java
@@ -8,17 +8,27 @@ public class Ej2 {
 		// TODO Auto-generated method stub
 		Scanner sc= new Scanner(System.in);
 		System.out.println("Hola, bienvenido a: Acierta la contraseña, por favor introduzca un numero");
+		String[] password;
 		String palabra=sc.next();
 		String palabra2="Contraseña";
 		boolean equal;
 		equal=palabra.equals(palabra2);;;;;;;;;;;;;;
 		//primero vemos la comparación de los dos
 		//vemos lo de las posiciones 
-		while(equal==false) {
+		do{
 		System.out.println("contraseña incorrecta");
 		palabra=sc.next();
 		equal=palabra.equals(palabra2);
+		
+		for (int i=0;i<password.length;i++) {
+			//el password aun no tiene valor
+			System.out.println(password.chatAt(i));
+			
 		}
+		
+		}while (equal==false);
+		
+		
 		
 	}
 	
