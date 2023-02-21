bdd73477019174fa4f3b56661ec3574e957021d2
enieves <enieves@106-02.info2>
2023-02-21 14:21:46 +0100

    crear comandos

---
+++ Tema6/src/prac/Ej5.java
@@ -0,0 +1,38 @@
+package prac;
+
+import java.util.Scanner;
+
+public class Ej5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*creamos el array que guardara las letras y irá aumentando cada vez que aumentas en 1 el numero de letras
+		es decir, cuando escribimos una palabra, el contador aumenta al numero de letras que tiene la palabra*/
+		Scanner sc= new Scanner(System.in);
+		System.out.println("Hola, bienvenido a: Acierta la contraseña, por favor introduzca un numero");
+		String[] guardado;
+		int [] tamano;
+		int letras=0;
+		String palabra=sc.next();
+		letras=palabra.length();
+		tamano= new int [letras];
+		
+		for(int i=0;i<tamano.length;i++) {
+			
+		}
+		
+		//cada vez que escribimos letra por letra, se va guardando en otr sitio
+		//se hace a partir de la lectura de lo escrito
+		
+		//sysout de guardado en cierta posicion mas el espacio
+		
+		
+		
+		//creamos un while que va aumentando un array segun el numero de letras que introducimos
+		
+		
+		//luego hacemos print de eso
+		
+	}
+
+}
