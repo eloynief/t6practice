71a4b139becc85fed88afb17c9bdfbfc0b3119d6
enieves <enieves@106-02.info2>
2023-02-23 14:37:24 +0100

    terminar la idea

--- Tema6/src/prac/Ej7.java
+++ Tema6/src/prac/Ej7.java
@@ -9,11 +9,19 @@ public class Ej7 {
 		Scanner sc= new Scanner(System.in);
 		String palabra,frase;
 		int veces=0;
-		System.out.println("introduzca una palabra");
 		String[] palabras;
+		System.out.println("introduzca una palabra");
 		
+		palabra=sc.next();
+		//si la mitad hacia arriba de esa parte de string array es igual que la otra mitad de abajo,entonces
 		
-		
+//		se guardan las palabras en el string palabras
+		palabras=palabra.split(" ");
+		//for donde i se iguala a palabras.length y disminuye hasta llegar a la mitad del palabras.length
+		//si en ese for i string es igual que otra variable que va sumando desde 0
+		for(int i=palabras.length;i>palabras.length/2;i--) {
+			
+		}
 	}
 
 }
